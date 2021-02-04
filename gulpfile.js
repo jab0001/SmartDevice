726e54875421a53cc796887f49773d1de777c137
jab001 <production@agima.ru>
2021-02-04 22:06:37 +0300

    Преимущеста, О компании

--- gulpfile.js
+++ gulpfile.js
@@ -63,7 +63,7 @@ gulp.task("images", function() {
 gulp.task("webp", function () {
   return gulp.src("source/img/**/*.{png,jpg}")
     .pipe(webp({quality: 90}))
-    .pipe(gulp.dest("source/img"));
+    .pipe(gulp.dest("build/img"));
 });
 
 gulp.task("sprite", function () {
@@ -97,5 +97,5 @@ gulp.task("clean", function () {
   return del("build");
 });
 
-gulp.task("build", gulp.series("clean", "copy", "css", "sprite", "html"));
+gulp.task("build", gulp.series("clean", "copy", "css","webp", "sprite", "html"));
 gulp.task("start", gulp.series("build", "server"));
