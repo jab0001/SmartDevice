15bef2705ad6fa17d4ba68dad855984f73002733
jab001 <production@agima.ru>
2021-02-05 22:02:35 +0300

    Форма обратной свзи, вадидация телефона, пп по проекту

--- source/js/App.js
+++ source/js/App.js
@@ -1,11 +1,15 @@
 "use strict";
 
 const footer = document.querySelector(".page-footer");
-const scrollBtn = document.querySelector('.banner__scroll');
+const scrollBtn = document.querySelector(".banner__scroll");
 const websiteBtn = footer.querySelector(".website__btn");
 const websiteMenu = footer.querySelector(".website__list");
 const officeBtn = footer.querySelector(".office__btn");
 const officeMenu = footer.querySelector(".office__list");
+const contactBtn = document.querySelector(".question-form__btn");
+const userName = document.querySelector("#name");
+const userPhone = document.querySelector("#phone");
+const userText = document.querySelector("#message");
 
 document.addEventListener("DOMContentLoaded", function (evt) {
   evt.preventDefault();
@@ -32,4 +36,14 @@ scrollBtn.addEventListener("click", function () {
     top: down.offsetTop - window.scrollY,
     behavior: "smooth",
   });
+});
+
+IMask(document.querySelector('#phone'), {mask: '+{7}(000)000-00-00'});
+
+contactBtn.addEventListener("click", function (evt) {
+  evt.preventDefault();
+
+  localStorage.setItem("user-name", userName.value);
+  localStorage.setItem("user-phone", userPhone.value);
+  localStorage.setItem("user-text", userText.value);
 });
