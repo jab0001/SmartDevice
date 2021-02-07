c3f606a6390d743af3081099948f177d9cc5c310
jab001 <production@agima.ru>
2021-02-07 16:34:06 +0300

    Еще правки по линтеру

--- source/js/App.js
+++ source/js/App.js
@@ -1,116 +1,131 @@
-"use strict";
+'use strict';
 
 (function () {
-  const footer = document.querySelector(".page-footer");
-  const bannerBtn = document.querySelector(".banner__btn");
-  const bannerScroll = document.querySelector(".banner__scroll");
-  const websiteBtn = footer.querySelector(".website__btn");
-  const websiteMenu = footer.querySelector(".website__list");
-  const officeBtn = footer.querySelector(".office__btn");
-  const officeMenu = footer.querySelector(".office__list");
-  const contactBtn = document.querySelector(".question-form__btn");
-  const userName = document.querySelector("#name");
-  const userPhone = document.querySelector("#phone");
-  const userPhoneModal = document.querySelector("#user-phone-modal");
-  const userText = document.querySelector("#message");
-  const openModalBtn = document.querySelector(".main-nav__btn");
-  const popup = document.querySelector(".modal");
-  const popupFog = document.querySelector(".fog");
-  const closeModalBtn = document.querySelector(".modal__btn-close");
+  var footer = document.querySelector('.page-footer');
+  var bannerBtn = document.querySelector('.banner__btn');
+  var bannerScroll = document.querySelector('.banner__scroll');
+  var websiteBtn = footer.querySelector('.website__btn');
+  var websiteMenu = footer.querySelector('.website__list');
+  var officeBtn = footer.querySelector('.office__btn');
+  var officeMenu = footer.querySelector('.office__list');
+  var contactBtn = document.querySelector('.question-form__btn');
+  var userName = document.querySelector('#name');
+  var userPhone = document.querySelector('#phone');
+  var userPhoneModal = document.querySelector('#user-phone-modal');
+  var userText = document.querySelector('#message');
+  var openModalBtn = document.querySelector('.main-nav__btn');
+  var popup = document.querySelector('.modal');
+  var popupFog = document.querySelector('.fog');
+  var closeModalBtn = document.querySelector('.modal__btn-close');
+  var contacts = document.querySelector('#contacts');
+  var advantages = document.querySelector('#advantages');
+  var nameInput = document.querySelector('#user-name-modal');
 
-  document.addEventListener("DOMContentLoaded", function (evt) {
+  var maskOptions = {
+    mask: '+{7}(000)000-00-00'
+  };
+
+  var maskNumber = function (phone, mask) {
+    return new window.IMask(phone, mask);
+  };
+
+  if (userPhone && userPhoneModal) {
+    maskNumber(userPhone, maskOptions);
+    maskNumber(userPhoneModal, maskOptions);
+  }
+
+  document.addEventListener('DOMContentLoaded', function (evt) {
     evt.preventDefault();
 
     if (websiteMenu) {
-      websiteMenu.classList.remove("website__list--no-js");
-      websiteMenu.classList.add("website--closed");
+      websiteMenu.classList.remove('website__list--no-js');
+      websiteMenu.classList.add('website--closed');
     }
     if (officeMenu) {
-      officeMenu.classList.remove("office__list--no-js");
-      officeMenu.classList.add("office--closed");
+      officeMenu.classList.remove('office__list--no-js');
+      officeMenu.classList.add('office--closed');
     }
   });
 
   if (websiteBtn && websiteMenu) {
-    websiteBtn.addEventListener("click", function (evt) {
+    websiteBtn.addEventListener('click', function (evt) {
       evt.preventDefault();
-      websiteMenu.classList.toggle("website--opened");
-      websiteBtn.classList.toggle("website__btn--remove");
+      websiteMenu.classList.toggle('website--opened');
+      websiteBtn.classList.toggle('website__btn--remove');
     });
   }
 
   if (officeBtn && officeMenu) {
-    officeBtn.addEventListener("click", function (evt) {
+    officeBtn.addEventListener('click', function (evt) {
       evt.preventDefault();
-      officeMenu.classList.toggle("office--opened");
-      officeBtn.classList.toggle("office__btn--remove");
+      officeMenu.classList.toggle('office--opened');
+      officeBtn.classList.toggle('office__btn--remove');
     });
   }
 
   if (bannerBtn) {
-    bannerBtn.addEventListener("click", function () {
+    bannerBtn.addEventListener('click', function () {
       window.scrollBy({
         top: contacts.offsetTop - window.scrollY,
-        behavior: "smooth",
+        behavior: 'smooth',
       });
     });
   }
 
   if (bannerScroll) {
-    bannerScroll.addEventListener("click", function () {
+    bannerScroll.addEventListener('click', function () {
       window.scrollBy({
         top: advantages.offsetTop - window.scrollY,
-        behavior: "smooth",
+        behavior: 'smooth',
       });
     });
   }
 
-  IMask(userPhone, { mask: "+{7}(000)000-00-00" });
-  IMask(userPhoneModal, { mask: "+{7}(000)000-00-00" });
 
   if (contactBtn) {
-    contactBtn.addEventListener("click", function (evt) {
+    contactBtn.addEventListener('click', function (evt) {
       evt.preventDefault();
 
-      localStorage.setItem("user-name", userName.value);
-      localStorage.setItem("user-phone", userPhone.value);
-      localStorage.setItem("user-text", userText.value);
+      localStorage.setItem('user-name', userName.value);
+      localStorage.setItem('user-phone', userPhone.value);
+      localStorage.setItem('user-text', userText.value);
     });
   }
 
   if (popup && popupFog) {
-    window.addEventListener("keydown", function (evt) {
+    window.addEventListener('keydown', function (evt) {
       if (evt.keyCode === 27) {
         evt.preventDefault();
-        if (popup.classList.contains("modal--active")) {
-          popup.classList.remove("modal--active");
-          popupFog.classList.remove("fog--active");
+        if (popup.classList.contains('modal--active')) {
+          popup.classList.remove('modal--active');
+          popupFog.classList.remove('fog--active');
         }
       }
     });
 
-    popupFog.addEventListener("click", function (evt) {
+    popupFog.addEventListener('click', function (evt) {
       evt.preventDefault();
-      if (popup.classList.contains("modal--active")) {
-        popup.classList.remove("modal--active");
-        popupFog.classList.remove("fog--active");
+      if (popup.classList.contains('modal--active')) {
+        popup.classList.remove('modal--active');
+        popupFog.classList.remove('fog--active');
       }
     });
 
     if (closeModalBtn) {
-      closeModalBtn.addEventListener("click", function (evt) {
+      closeModalBtn.addEventListener('click', function (evt) {
         evt.preventDefault();
-        if (popup.classList.contains("modal--active")) {
-          popup.classList.remove("modal--active");
-          popupFog.classList.remove("fog--active");
+        if (popup.classList.contains('modal--active')) {
+          popup.classList.remove('modal--active');
+          popupFog.classList.remove('fog--active');
         }
       });
     }
-    if (openModalBtn) {
-      openModalBtn.addEventListener("click", function (event) {
+    if (openModalBtn && nameInput) {
+      openModalBtn.addEventListener('click', function (event) {
         event.preventDefault();
-        popup.classList.add("modal--active");
-        popupFog.classList.add("fog--active");
+        popup.classList.add('modal--active');
+        popupFog.classList.add('fog--active');
+        nameInput.focus();
       });
     }
   }
